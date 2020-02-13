dad2077b0c92d56c52d94ff72abd83bde67e57f0
Ruben Hooijer <rubenhooijer16@gmail.com>
2020-02-13 17:45:50 +0100

    Added A* pathfinding

--- Astar/Assets/Scripts/Astar.cs
+++ Astar/Assets/Scripts/Astar.cs
@@ -16,7 +16,155 @@ public class Astar
     /// <returns></returns>
     public List<Vector2Int> FindPathToTarget(Vector2Int startPos, Vector2Int endPos, Cell[,] grid)
     {
-        return null;
+        List<Node> nodeGrid = new List<Node>();
+
+        Vector2Int gridSize = new Vector2Int(grid.GetLength(0), grid.GetLength(1));
+        for (int x = 0; x < gridSize.x; x++)
+        {
+            for (int y = 0; y < gridSize.y; y++)
+            {
+                Vector2Int pos = new Vector2Int(x, y);
+                nodeGrid.Add(new Node(pos, null, int.MaxValue, CalculateDistanceCost(pos, endPos)));
+            }
+        }
+
+        Node startNode = nodeGrid.Find(cell => cell.position == startPos);
+        startNode.GScore = 0;
+
+        List<Node> openList = new List<Node>();
+        List<Node> closedList = new List<Node>();
+
+        openList.Add(startNode);
+
+        while (openList.Count > 0)
+        {
+            Node current = GetLowestFScoreNode(openList);
+            if (current.position == endPos)
+            {
+                Debug.Log("met endpos");
+                break;
+            }
+
+            openList.Remove(current);
+
+            closedList.Add(current);
+            List<Node> neighbours = GetNeighbours(current, grid, nodeGrid, closedList);
+            Debug.Log(neighbours.Count);
+
+            foreach (Node neighbour in neighbours)
+            {
+                int tentativeGScore = (int)current.GScore + CalculateDistanceCost(current.position, neighbour.position);
+                if (tentativeGScore < neighbour.GScore)
+                {
+                    neighbour.parent = current;
+                    neighbour.GScore = tentativeGScore;
+
+                    if (!openList.Contains(neighbour))
+                    {
+                        openList.Add(neighbour);
+                    }
+                }
+            }
+        }
+
+        Node endNode = nodeGrid.Find(cell => cell.position == endPos);
+        if (endNode.parent == null)
+        {
+            return null;
+        }
+
+        return CalculatePath(startNode, endNode);        
+    }
+
+    private List<Vector2Int> CalculatePath(Node startNode, Node endNode)
+    {
+        if (endNode.parent == null)
+        {
+            return null;
+        }
+
+        List<Vector2Int> path = new List<Vector2Int>();
+
+        Node cNode = endNode;
+
+        while (cNode.parent != null)
+        {
+            path.Add(cNode.position);
+            cNode = cNode.parent;
+        }
+
+        path.Reverse();
+        return path;
+    }
+
+    private Node GetLowestFScoreNode(List<Node> openList)
+    {
+        Node lowestFNode = openList[0];
+
+        for (int i = 1; i < openList.Count; i++)
+        {
+            Node cNode = openList[i];
+            if (cNode.FScore < lowestFNode.FScore)
+            {
+                lowestFNode = cNode;
+            }
+        }
+
+        return lowestFNode;
+    }
+
+    private bool CanWalkToCell(Cell fromCell, Cell toCell)
+    {
+        if (fromCell.gridPosition.x > toCell.gridPosition.x)
+        {
+            return !(fromCell.HasWall(Wall.LEFT) && toCell.HasWall(Wall.RIGHT));
+        } else if(fromCell.gridPosition.x < toCell.gridPosition.x)
+        {
+            return !(fromCell.HasWall(Wall.RIGHT) && toCell.HasWall(Wall.LEFT));
+        } else if(fromCell.gridPosition.y > toCell.gridPosition.y)
+        {
+            return !(fromCell.HasWall(Wall.DOWN) && toCell.HasWall(Wall.UP));
+        } else
+        {
+            return !(fromCell.HasWall(Wall.UP) && toCell.HasWall(Wall.DOWN));
+        }
+    }
+
+    private int CalculateDistanceCost(Vector2Int cPos, Vector2Int endPos)
+    {
+        int xDistance = Mathf.Abs(cPos.x - endPos.x);
+        int yDistance = Mathf.Abs(cPos.y - endPos.y);
+
+        return xDistance + yDistance;
+    }
+
+    private List<Node> GetNeighbours(Node cell, Cell[,] grid, List<Node> nodeGrid, List<Node> closedList)
+    {
+        List<Node> result = new List<Node>();
+        for (int x = -1; x < 2; x++)
+        {
+            for (int y = -1; y < 2; y++)
+            {
+                int cellX = cell.position.x + x;
+                int cellY = cell.position.y + y;
+                if (cellX < 0 || cellX >= grid.GetLength(0) || cellY < 0 || cellY >= grid.GetLength(1) || Mathf.Abs(x) == Mathf.Abs(y))
+                {
+                    continue;
+                }
+                if (!CanWalkToCell(grid[cell.position.x, cell.position.y], grid[cellX, cellY]))
+                {
+                    continue;
+                }
+                Vector2Int canditatePos = new Vector2Int(cellX, cellY);
+                Node candidateNode = nodeGrid.Find(node => node.position == canditatePos);
+                if (candidateNode != null && !closedList.Contains(candidateNode))
+                {
+                    result.Add(candidateNode);
+                }
+            }
+        }
+
+        return result;
     }
 
     /// <summary>
